4e3425aa299a18febc576527b7a9e02e3d5b1bf1
codeinvisible <lamasandesh400@gmail.com>
2022-07-07 15:32:57 +0545

    final commit

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,31 +1,25 @@
-import React from 'react'
-import {useSelector} from 'react-redux';
-import UserProfile from '../../pages/UserProfile';
-import { Sidebar } from '..';
+import React from "react";
+import { useSelector } from "react-redux";
+import UserProfile from "../../pages/UserProfile";
+import { Sidebar } from "..";
 const MainPage = () => {
-    const {user, userSelected} = useSelector(state => state.users);
-    return (
-        
+  const { user, userSelected } = useSelector((state) => state.users);
+  return (
+    <>
+      <Sidebar />
+      {Object.keys(user).length === 0 && !userSelected ? (
         <>
-            <Sidebar />
-            {
-                Object.keys(user).length === 0 && !userSelected ?(<>
-               
-                    <div className="relative w-screen">
-                        <div className="w-1/2 m-auto text-center my-72">
-                            <h1 className="text-6xl font-bold">INTELZY</h1>
-                            <p>PROJECT</p>
-                        </div>
-                    </div>
-                    </>):(
-                        <>
-                            <UserProfile />
-                        </>
-                    )
-            }
+          <div className="relative w-screen">
+            <div className="w-1/2 m-auto text-center my-72"></div>
+          </div>
         </>
-        
-    )
-}
+      ) : (
+        <>
+          <UserProfile />
+        </>
+      )}
+    </>
+  );
+};
 
-export default MainPage
+export default MainPage;
